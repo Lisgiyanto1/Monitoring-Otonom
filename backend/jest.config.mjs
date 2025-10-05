3e3086a8bf40cfbe36e4c8588f8f9456c831769f
Lisgiyanto1 <lisgiyantosofiyan@gmail.com>
2025-10-06 00:20:32 +0700

    test: solve3

--- backend/jest.config.mjs
+++ backend/jest.config.mjs
@@ -1,16 +1,9 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-
-  // +++++++++++++++++++++++ TAMBAHKAN BARIS INI +++++++++++++++++++++++
-  // Secara eksplisit memberitahu Jest untuk hanya mencari tes di dalam direktori
-  // 'src' atau 'test' yang ada di dalam folder backend ini.
-  testMatch: ['<rootDir>/src/**/*.test.ts', '<rootDir>/test/**/*.test.ts'],
-  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: 'tsconfig.json', 
-    }],
-  },
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
+  testMatch: ['**/*.test.ts'],
 };
